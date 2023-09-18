697595e9411711ace9acea309a01f599dca96636
kevin zhou <kevinzhou2@live.cn>
2023-09-17 20:11:38 -0700

    implement the dataloader

--- utils.py
+++ utils.py
@@ -1,3 +1,23 @@
+
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+import torch
+
 def accuracy_score(all_labels, all_preds):
     #TODO
     pass
+
+def tokenize(text_data, tokenizer, max_length, padding = True):
+    
+    encoding = tokenizer(text_data, return_tensors='pt', padding=padding, truncation = True, max_length = max_length)
+    input_ids = encoding['input_ids']
+    attention_mask = encoding['attention_mask']
+    return input_ids, attention_mask
+
+
+def get_Dataset(dataset, tokenizer,max_length):
+    premise_hypothesis = [premise+'<\s>'+hypothesis for premise,hypothesis in zip(dataset['premise'],dataset['hypothesis'])]
+    label  = torch.Tensor(dataset['label'])
+    token_ids, token_attn = tokenize(premise_hypothesis, tokenizer, max_length = max_length)
+    train_data = TensorDataset(token_ids, token_attn, label)
+    return train_data #needed for T5
