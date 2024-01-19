70bde0dc41e48374f1ac71981a8c792ffb349a84
DeqingFu <deqingfu@usc.edu>
2024-01-19 15:59:44 -0800

    relu bug fix

--- modeling_roberta_relu.py
+++ modeling_roberta_relu.py
@@ -263,7 +263,7 @@ class RobertaSelfAttention(nn.Module):
             attention_scores = attention_scores + attention_mask
 
         # Normalize the attention scores to probabilities.
-        attention_probs = nn.functional.relu(attention_scores, dim=-1)
+        attention_probs = nn.functional.relu(attention_scores)
 
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
